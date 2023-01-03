f1d9cefdef0d686ea36ed360ab6d6a2e9ef1d111
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-04 02:11:23 +0300

    checks if a random +ve, -ve or zero

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+# if the number is greater than 0: is positive
+if number > 0:
+    print("{:d} is positive".format(number))
+# if the number is 0: is zero
+elif number == 0:
+    print("{:d} is zero".format(number))
+# if the number is less than 0: is negative
+else:
+    print("{:d} is negative".format(number))
