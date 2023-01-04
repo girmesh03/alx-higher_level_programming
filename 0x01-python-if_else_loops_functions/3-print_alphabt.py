525e8c3229894e0bf73cf6c7b43d9bbcbf34c8cc
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-04 03:00:10 +0300

    bug is fixed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -5,5 +5,5 @@
 # except for 'q' and 'e'
 
 for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{:c}".format(i), end="")
+    if chr(i) != 'e' and chr(i) != 'q':
+        print("{}".format(chr(i)), end="")
