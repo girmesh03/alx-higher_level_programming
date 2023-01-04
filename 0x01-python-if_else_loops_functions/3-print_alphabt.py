77bb4207fb9e87d84d3fa61482063c78a48017c5
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-04 03:04:10 +0300

    variable name and char position is changed

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,6 +4,6 @@
 # in lowercase, not followed by a new line.
 # except for 'q' and 'e'
 
-for i in range(97, 123):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end="")
+for character in range(97, 123):
+    if chr(character) != 'q' and chr(character) != 'e':
+        print("{}".format(chr(character)), end="")
