c6585454b1c72e04d7eb9d6de0c2b1547e72e200
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-09 00:21:31 +0300

    variable assignement changed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
 
-    biggest = 0
+    biggest = my_list[0]
     for index in range(len(my_list)):
         if my_list[index] > biggest:
             biggest = my_list[index]
