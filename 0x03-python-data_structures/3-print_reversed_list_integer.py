0d09e19cc6b95c34ce0d80b97ad9c7bdf15fa44e
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-08 19:48:29 +0300

    else part added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
         pass
-
-    for value in reversed(my_list):
-        print("{}".format(value))
+    else:
+        for value in reversed(my_list):
+            print("{}".format(value))
