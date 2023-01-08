12e139e69155fe0a07cc3efb33dfa6e90626145d
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-09 00:17:16 +0300

    empty case changed to list length

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest = 0
 
-    if my_list == list():
+    if len(my_list) == 0:
         return None
 
+    biggest = 0
     for index in range(len(my_list)):
         if my_list[index] > biggest:
             biggest = my_list[index]
