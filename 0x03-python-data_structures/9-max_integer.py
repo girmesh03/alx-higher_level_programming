e489a5546cfd63871df51472ef1afda00c178a34
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-09 00:14:53 +0300

    empty case added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     biggest = 0
+
+    if my_list == list():
+        return None
+
     for index in range(len(my_list)):
         if my_list[index] > biggest:
             biggest = my_list[index]
