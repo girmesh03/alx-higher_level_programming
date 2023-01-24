c1379b5071afb3dbfb29850620c1a44da76623a3
girmesh03 <girmachewzewdie03@gmail.com>
2023-01-25 00:44:36 +0300

    raises an exception if size not int or less than 0

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a square by: (based on 1-square.py)."""
+
+
+class Square:
+    """This is a constructor for the Square class
+    which initializes the size instance attribute and
+    raises an exception if the size is not an integer
+    or if the size is less than 0"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
