88747db8561f2117dff8c808728e6a757b2e4a9a
girmesh03 <girmachewzewdie03@gmail.com>
2023-02-08 00:30:20 +0300

    added files and task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module that defines a read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str): The name of the file to read
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
