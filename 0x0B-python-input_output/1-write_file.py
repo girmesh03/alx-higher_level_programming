52cd8dfba6311d8095aca0ba6ef17207419eaf4e
girmesh03 <girmachewzewdie03@gmail.com>
2023-02-08 01:33:23 +0300

    file doc added for task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,14 @@
 
 
 def write_file(filename="", text=""):
-    """A function that writes a string to a text file"""
+    """A function that writes a string to a text file
+
+    Args:
+        filename (str): The name of the file to write to
+        text (str): The string to write to the file
+
+    Returns:
+        The number of characters written
+    """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
