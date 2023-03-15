4bac0d74320aab07453e8c192d0824410652ac46
girmesh03 <girmachewzewdie03@gmail.com>
2023-03-15 03:48:02 +0300

    print full disc. of table updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- prints the full description of the table
 -- first_table from the database hbtn_0c_0 in your MySQL sevrer.
 
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
