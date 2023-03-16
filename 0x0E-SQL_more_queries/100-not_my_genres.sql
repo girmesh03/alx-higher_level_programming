e04536135d89c20c518a44cbc083fa0f445aee8f
girmesh03 <girmachewzewdie03@gmail.com>
2023-03-16 05:14:24 +0300

    100-not_my_genres.sql updated

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,11 +4,12 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use a maximum of two SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name,
-	SUM(tv_shows_ratings.rating) AS rating_sum
+SELECT tv_genres.name
 FROM tv_genres
-	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	JOIN tv_shows_ratings ON tv_shows.id = tv_shows_ratings.show_id
-GROUP BY tv_genres.id
-ORDER BY rating_sum ASC;
+	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id NOT IN (
+		SELECT id
+		FROM tv_shows
+		WHERE title = 'Dexter'
+	)
+ORDER BY tv_genres.name ASC;
