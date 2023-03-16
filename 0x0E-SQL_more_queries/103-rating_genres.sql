b7d991ad169f0f96f10f4448ec04d6a1bc7a9018
girmesh03 <girmachewzewdie03@gmail.com>
2023-03-16 05:39:40 +0300

    103-rating_genres.sql updated 6

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,14 +3,12 @@
 -- Results must be sorted in descending order by their rating
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT CONCAT(
-		tv_genres.name,
-		' - ',
-		IFNULL(SUM(tv_show_ratings.rate), 0)
-	) AS 'tv_genres.name - rating sum'
+SELECT tv_genres.name,
+	COALESCE(SUM(tv_show_ratings.rate), 0) AS rating_sum
 FROM tv_genres
 	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 	LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+	LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY SUM(tv_show_ratings.rate) DESC;
+ORDER BY rating_sum DESC,
+	tv_genres.name ASC;
