11969786af7f9238a2d18a8468422007d74d8e9d
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-11 03:00:29 +0300

    code updated

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 // prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
 const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < stringArray.length; i++) {
-  console.log(stringArray[i]);
+for (const str of stringArray) {
+  console.log(str);
 }
