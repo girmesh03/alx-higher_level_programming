d71c29e54eeb805314a1e35ed794f7c20459c119
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-12 02:57:25 +0300

    9-logme.js task is added

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Write a function that prints the number of arguments already printed and the new argument value. (see example below)
+
+Prototype: exports.logMe = function (item)
+Output format: <number arguments already printed>: <current argument value></current> */
+
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+  console.log(this.count + ': ' + item);
+  this.count++;
+};
