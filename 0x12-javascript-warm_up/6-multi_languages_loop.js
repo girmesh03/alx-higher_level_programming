f628927e4b185184b423a9200785da131c1fecbb
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-11 03:05:23 +0300

    code updated 2

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints 3 lines: by using an array of string and a loop
 
-for (const str of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(str);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
