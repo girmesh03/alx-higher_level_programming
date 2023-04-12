63aef839f8df900e574c961918c608e4004a394f
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-12 03:22:45 +0300

    100-map.js task 11 is added

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Write a script that imports an array and computes a new array.
+
+Your script must import list from the file 100 - data.js
+You must use a map.Tips
+A new list must be created with each value equal to the value of the initial list, multipled by the index in the list
+Print both the initial list and the new list */
+
+const list = require('./tests/100-data.js').list;
+console.log(list);
+console.log(list.map((x, index) => x * index));
