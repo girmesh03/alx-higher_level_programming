a8793c2ae2752ee7db054427da3d44480db91e74
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-16 07:28:33 +0300

    relationship module is added

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """This module defines a City class that inherits from Base"""
 
+from relationship_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class City(Base):
-    """Class that represents a city"""
+    """
+    Class that represents a city
+    """
 
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """This module defines a relationship"""
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Class that represents a state"""
+    """
+    Class that represents a state
+    """
 
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
