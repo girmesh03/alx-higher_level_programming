d8e930605e2ec0dc3d39a0fb7a49c6ac166d4fe0
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-20 03:15:06 +0300

    task 4, 6, 7, 8, 9 modified

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,8 +2,13 @@
 -- in the current database in your MySQL server
 -- if table first_table already exists, it will not fail.
 
+-- if u want to add a primary key constraint to the table, use:
+-- ALTER TABLE first_table ADD PRIMARY KEY (id);
+
+-- if u want to remove a primary key constraint from the table, use:
+-- ALTER TABLE first_table DROP PRIMARY KEY;
+
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
-	name VARCHAR(256),
-	PRIMARY KEY (id)
+	name VARCHAR(256)
 );
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,5 @@
--- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- displays the number of records with id = 89 in the table first_table
+-- of the database hbtn_0c_0 in your MySQL server.
 
-SELECT COUNT(id) FROM first_table WHERE id = 89;
+-- select all rows from first_table in which the id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
