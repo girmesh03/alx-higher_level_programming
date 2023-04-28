ebf6339f9524377273a8fd7b2ec5e1f2c72448fd
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-28 20:23:57 +0300

    0-hbtn_status.py update 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as resp:
         data = resp.read()
-        print("Body response:")
-        print(f"\t- type: {type(data)}")
-        print(f"\t- content: {data}")
-        print(f"\t- utf8 content: {data.decode('utf-8')}")
+    print("Body response:")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {data.decode('utf-8')}")
