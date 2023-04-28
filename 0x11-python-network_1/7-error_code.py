a5c555232530251205be71e851c8ec7c786dbba5
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-28 20:39:19 +0300

    7-error_code.py is added

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,19 +4,14 @@ Python script that takes in a URL, sends a request to
 the URL and displays the body of the response.
 """
 
-import urllib.request
-import urllib.parse
+import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
 
-    values = {'email': email}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
