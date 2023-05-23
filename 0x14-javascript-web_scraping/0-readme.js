e518a0d10491df0802c86e7c59a3aaaad4483c61
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 01:01:23 +0300

    0-readme.js update 5

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,11 +7,11 @@ const file = process.argv[2];
 
 // Read the contents of the file asynchronously
 fs.readFile(file, 'utf-8', (err, data) => {
-	if (err) {
-		// If there was an error reading the file, log the error object to the console
-		console.error(err);
-		return;
-	}
-	// If the file was read successfully, log its contents to the console
-	console.log(data);
+  if (err) {
+    // If there was an error reading the file, log the error object to the console
+    console.error(err);
+    return;
+  }
+  // If the file was read successfully, log its contents to the console
+  console.log(data);
 });
