25dce5a9df166a9038edeb2d21fc25aa26548e98
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 02:11:06 +0300

    101-starwars_characters.js update 6

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-// A script that prints all characters of a Star Wars movie synchronously
-
-// Require the request module
+// prints all characters of a Star Wars movie synchronously
 const request = require('request');
 
 // Get movie ID from command line argument
 const movieId = process.argv[2];
 
 // Construct URL to fetch movie data
-const url = `https://swapi.dev/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 // Make a request to fetch movie data
 request(url, (error, response, body) => {
@@ -18,38 +16,36 @@ request(url, (error, response, body) => {
     return;
   }
 
-  // Parse movie data
-  const film = JSON.parse(body);
+  // Parse JSON string into object
+  const movie = JSON.parse(body);
 
-  // Get URLs of characters in the movie
-  const charactersUrls = film.characters;
+  // Get characters from movie object
+  const characters = movie.characters;
 
-  // Fetch and print the character names synchronously
-  fetchAndPrintCharacters(charactersUrls);
+  // Create an array of Promises that will be resolved when each character is fetched
+  const characterPromises = characters.map(character => {
+    return new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        // Handle errors
+        if (error) {
+          reject(error);
+        } else {
+          // Resolve with character name
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
+  });
+
+  // Resolve all Promises
+  Promise.all(characterPromises)
+    .then(characters => {
+      // Print characters
+      characters.forEach(character => console.log(character));
+    })
+    .catch(error => {
+      console.error(error);
+    }
+    );
 });
 
-function fetchAndPrintCharacters(urls) {
-  if (urls.length === 0) {
-    return;
-  }
-
-  const characterUrl = urls.shift();
-
-  // Make a request to fetch character data
-  request(characterUrl, (error, response, body) => {
-    // Handle errors
-    if (error) {
-      console.error('Error:', error);
-      return;
-    }
-
-    // Parse character data
-    const character = JSON.parse(body);
-
-    // Print the name of the character
-    console.log(character.name);
-
-    // Fetch and print the next character name
-    fetchAndPrintCharacters(urls);
-  });
-}
