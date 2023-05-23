5aef88ddfd31f9a349ccd1e7fa270fd58815547e
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 01:14:29 +0300

    comment modified on task 0 and 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Write a script that writes a string to a file.
+// A script that writes a string to a file.
 const fs = require('fs');
 
 // The first argument is the file path
