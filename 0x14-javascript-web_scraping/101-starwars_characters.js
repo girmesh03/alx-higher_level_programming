21cab846c46db6344756fa40a33d09b855787afb
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 02:18:26 +0300

    101-starwars_characters.js update 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -12,43 +12,38 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request(url, (error, response, body) => {
   // Handle errors
   if (error) {
-    console.error('Error:', error);
+    console.error(error);
     return;
   }
 
-  // Parse movie data
-  const film = JSON.parse(body);
+  // Parse JSON string into object
+  const movie = JSON.parse(body);
 
-  // Get URLs of characters in the movie
-  const charactersUrls = film.characters;
+  // Get characters from movie object
+  const characters = movie.characters;
 
-  // Create an array of promises that will fetch the data of each character
-  const charactersPromises = charactersUrls.map(characterUrl => {
-    // Create a promise that will fetch the data of a character
+  // Create an array of Promises that will be resolved when each character is fetched
+  const characterPromises = characters.map(character => {
     return new Promise((resolve, reject) => {
-      // Make a request to fetch character data
-      request(characterUrl, (error, response, body) => {
+      request(character, (error, response, body) => {
         // Handle errors
         if (error) {
           reject(error);
-          return;
+        } else {
+          // Resolve with character name
+          resolve(JSON.parse(body).name);
         }
-
-        // Parse character data
-        const character = JSON.parse(body);
-
-        // Resolve the promise with the name of the character
-        resolve(character.name);
       });
     });
   });
 
-  // Wait for all promises to resolve and then print the names of the characters
-  Promise.all(charactersPromises)
+  // Resolve all Promises
+  Promise.all(characterPromises)
     .then(characters => {
       console.log(characters.join('\n'));
     })
     .catch(error => {
-      console.error('Error:', error);
+      console.error(error);
     });
 });
+
