e46e8ea424843e94148b4a6e03fef772d7c9bafb
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 01:10:40 +0300

    1-writeme.js is added

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// Write a script that writes a string to a file.
+const fs = require('fs');
+
+// The first argument is the file path
+const file = process.argv[2];
+
+// The second argument is the string to write
+const string = process.argv[3];
+
+// Write the string to the file
+fs.writeFile(file, string, 'utf-8', (err) => {
+  // If an error occurred during the writing, print the error object and return
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
