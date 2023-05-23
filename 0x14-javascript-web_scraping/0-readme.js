6530aba11931152d81adacd07e9ed3c366e0711c
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 00:57:40 +0300

    0-readme.js update 2

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 // Write a script that reads and prints the content of a file.
-
-// The first argument is the file path
 const fs = require('fs');
 // The content of the file must be read in utf-8
 const file = process.argv[2];
 
 // Read the file and print its content
-fs.readFile(file, 'utf8', (err, data) => {
+fs.readFile(file, 'utf-8', (err, data) => {
 	// If an error occurred during the reading, print the error object and return
 	if (err) {
-		console.log(err);
+		console.error(err);
 		return;
 	}
 	// otherwise, print the content of the file
