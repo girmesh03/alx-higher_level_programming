e5e79753e0b54607038953cce5d6d4b0890ba69d
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-24 00:49:01 +0300

    0-readme.js is added

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data);
+});
